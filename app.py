dfc3a262e345e76ce2d8e079a49ad84e0ce95181
24f3002331 <24f3002331@ds.study.iitm.ac.in>
2025-10-04 18:26:16 +0530

    Milestone-HMS DB-Relationship

---
+++ app.py
@@ -0,0 +1,26 @@
+## bcrypt
+
+from flask import Flask , render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db'
+app.config['SECRET_KEY'] = '250f7f1559a72d593f94f8814fab426f6aca7ec16e1e437c8d2ed6380ec0df8e'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+from models import *
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,57 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(80),unique=True,nullable=False)
+    password_hash = db.Column(db.String(120),nullable=False)
+    role = db.Column(db.String(20),nullable=False)
+
+class Doctor(db.Model):
+    __tablename__ = 'doctors'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(100),nullable=False)
+    department_id = db.Column(db.Integer,db.ForeignKey('departments.id'),nullable=False)
+    experience = db.Column(db.Integer,nullable=False)
+    specialization = db.Column(db.String(100),nullable=False)
+    description = db.Column(db.Text,nullable=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
+    appointments = db.relationship('Appointment',backref='doctor',lazy=True)
+
+class Department(db.Model):
+    __tablename__ = 'departments'
+    id = db.Column(db.Integer,primary_key=True,)
+    name = db.Column(db.String(100),nullable=False,unique=True)
+    doctors = db.relationship('Doctor',backref='department',lazy=True)
+    
+class Patient(db.Model):
+    __tablename__ = 'patients'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(100),nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
+    appointments = db.relationship('Appointment',backref='patient',lazy=True)
+class Appointment(db.Model):
+    __tablename__ = 'appointments'
+    id = db.Column(db.Integer,primary_key=True)
+    doctor_id = db.Column(db.Integer,db.ForeignKey('doctors.id'),nullable=False) 
+    patient_id = db.Column(db.Integer,db.ForeignKey('patients.id'),nullable=False)
+    appointment_date = db.Column(db.DateTime,nullable=False)
+    status = db.Column(db.String(20),default='Booked')
+    treatment = db.relationship("Treatment",backref="appointment",uselist=False)
+class Treatment(db.Model):
+    __tablename__ = 'treatments'
+    id = db.Column(db.Integer,primary_key=True)
+    appointment_id = db.Column(db.Integer,db.ForeignKey('appointments.id'),nullable=False)
+    diagnosis = db.Column(db.Text, nullable=False)
+    notes = db.Column(db.Text,nullable=False)
+    prescription = db.Column(db.Text)
+
+class Availability(db.Model):
+    __tablename__ = 'availabilities'
+    id = db.Column(db.Integer, primary_key=True)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctors.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+    is_booked = db.Column(db.Boolean, default=False, nullable=False)
